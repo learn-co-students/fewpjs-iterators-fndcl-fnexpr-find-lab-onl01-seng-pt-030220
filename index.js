cf281ca05108f22a83780f99827a4227676d30e0
Rkebede <rutht.kebede@gmail.com>
2020-09-13 12:39:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const testVar = {}
+function superbowlWin(arr) {
+  let win = arr.find(yearResult => yearResult.result === "W")
+  if (win) {
+    return win.year
+  }
 
-function testFunc() {
-  return "hi"
 }
