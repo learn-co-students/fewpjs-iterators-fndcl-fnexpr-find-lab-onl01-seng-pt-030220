d481a3f178c6438a84d6bc1af9bda37ac55dca54
aredmond85 <aredmond85@gmail.com>
2020-09-19 10:45:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-const testVar = {}
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
 
-function testFunc() {
-  return "hi"
+let superbowlWin = (record) => {
+  let result = record.find(record => record.result === 'W')
+  return !!result ? result.year : undefined
 }
+
+
+// const testVar = {}
+
+// function testFunc() {
+//   return "hi"
+// }
+
+// [1,3,5,6,8].find(function(e) { return e % 2 === 0 })
+
+// let roommates = ["jess", "winston", "winston", "nick"];
+// roommates.find( function(s) { return s === "winston" })
+// //=> winston
+// roommates.filter(function(s) { return s === "winston" })
+// //=> [winston, winston]
