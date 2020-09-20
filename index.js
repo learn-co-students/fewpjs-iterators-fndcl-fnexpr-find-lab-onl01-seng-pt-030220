3610a65b156c7ba8651eaa4ac93629bd66fa4696
github username <github email address>
2020-09-20 16:21:48 +0100

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
-const testVar = {}
 
-function testFunc() {
-  return "hi"
+
+
+// function superbowlWin(arrayObj){
+// arrayObj.find(findWin)
+// }; 
+
+// function findWin(object){
+// if (object.result === "W"){
+// return object.year} 
+// else{
+//   return undefined}
+
+// }; 
+
+
+function superbowlWin(obj){
+  const result = obj.find(result => 
+      {
+      return result.result === "W"
+      }
+      )
+if (result) {
+  return result.year
+} else {
+  return undefined
 }
+
+};
+
+// function superbowlWin(record){
+//   let win = record.find( ({ result}) => result === "W");
+//   // if (win){
+//   //   return win.year
+//   // } else {
+//   //   return undefined
+//   // }
+//   return win ? win.year : undefined
+// }
