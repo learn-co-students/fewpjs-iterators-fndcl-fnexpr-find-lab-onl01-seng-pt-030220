a5d95852a396bcceacf1f5396271ec7f4c051a08
antimonysayz <antimonysayz@gmail.com>
2020-09-20 22:27:37 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(record){
+  let win = record.find(record => record.result === "W")
+  return win ? win.year : undefined;
+}
