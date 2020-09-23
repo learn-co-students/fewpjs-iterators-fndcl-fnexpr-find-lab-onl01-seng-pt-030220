07190e4f1311712c8970848582fcb06f4f01cd6e
Haloren <mckennakyler@gmail.com>
2020-09-22 22:33:59 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let result = array.find(e => e.result === "W")
+  console.log(result)
+  if (result) {
+    // console.log(result)
+    return result.year;
+  } else {
+    return undefined
+  };
+
+  }
+  
+
+superbowlWin(record);
