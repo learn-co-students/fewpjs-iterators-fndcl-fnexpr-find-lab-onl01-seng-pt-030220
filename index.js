7c8c7ad9f2913a8b8761b0eaf1599a87fa390ab2
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-09-27 17:26:47 -0700

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 
-const record = [
-  {year: "2018", result: "L"},
-  {year: "2017", result: "W"},
-  {year: "2016", result: "N/A"}
-]
 
 function superbowlWin(record){
   let winningTeam = record.find( function(team) { return team.result === "W"})
@@ -11,5 +6,3 @@ function superbowlWin(record){
     return winningTeam.year
   }
 }
-
-superbowlWin(record)
