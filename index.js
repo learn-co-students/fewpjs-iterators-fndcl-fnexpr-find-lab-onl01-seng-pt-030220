610c58c5fbe05f748f89be526396d2a3f7b5e65f
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-09-27 17:35:59 -0700

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 
 
 function superbowlWin(record){
-  let winningTeam = record.find( function(team) { return team.result === "W"})
-  if (winningTeam){
-    return winningTeam.year
-  }
+  let winningTeam = record.find( team => team.result === "W" )
+  return !!winningTeam ? winningTeam.year : undefined
 }
+
+// function superbowlWin(record){
+//   let winningTeam = record.find( function(team) { return team.result === "W"})
+//   if (winningTeam){
+//     return winningTeam.year
+//   }
+// }
