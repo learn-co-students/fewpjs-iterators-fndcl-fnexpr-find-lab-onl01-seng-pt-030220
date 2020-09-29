22f295457e3b072e5fe29e2d2c17c7307ca81377
github username <github email address>
2020-09-28 19:02:20 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+//Flatiron's solution
+superbowlWin = (record) => {
+  let result = record.find( record => record.result === "W" )
+  return !!result ? result.year : undefined
+}
+
+
+
+// function superbowlWin(object) {
+//   let result = object.find(year, result)
+//   if result === "W"
+//     return result.year
+//   else 
+//     undefined 
+  
+// }
